671804c2ddac4be1fa652cb4a2d4e5da4a9a09b3
masikotimo <masikotimo@gmail.com>
2021-01-22 02:13:44 +0300

    gotten headings of H6 AND h5

--- page.sh
+++ page.sh
@@ -2,11 +2,11 @@
 function displayoutput() {
     arg1=$1
     
-    page="$(curl $arg1 | grep 'h6\|h5\|h4'  )"
+    page="$(curl $arg1 | grep -iPo '(?<=<h6>)(.*)(?=</h6>)'\|'(?<=<h5>)(.*)(?=</h5>)'  )"
     
     
     echo "$page"
 
-    # grep '<h6>' $page
+    # grep '<h4>' $page
 }
 displayoutput https://www.monitor.co.ug/uganda
