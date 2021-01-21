246fb5f3b9ce1fed905416e915a4f8a0228b0415
masikotimo <masikotimo@gmail.com>
2021-01-22 00:34:40 +0300

    written functions with arguments

--- firstbash.sh
+++ firstbash.sh
@@ -1,1 +1,10 @@
 echo "This statement will print out to the screen."
+
+echo "from the website https://www.monitor.co.ug/uganda"
+echo "=========="
+echo "List some files:" 
+ls
+touch testfile.txt
+uname -r
+echo "Today's date is `date`, this is week `date +"%V"`."
+printf "This is information provided by mysystem.sh. Program starts now.\n"
---
+++ functions.sh
@@ -0,0 +1,8 @@
+function displayoutput() {
+    arg1=$1
+    arg2=$2
+    
+    
+    echo "$arg1 and $arg2"
+}
+displayoutput www//ererererer bar
